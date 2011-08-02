ed51a0390a9b6151aea9760a0d1ad2c5191772e8
yunex76 <yunex76@bfb284a4-e940-11de-91d6-89caad0352f8>
2011-08-02 04:23:22 +0000

    Tutorial 05 release

---
+++ 0.0.05/src/com/yunex/Restaurant.java
@@ -0,0 +1,31 @@
+package com.yunex;
+
+public class Restaurant {
+
+	private String name = "";
+	private String address = "";
+	private String type = "";
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return getName();
+	}
+}
