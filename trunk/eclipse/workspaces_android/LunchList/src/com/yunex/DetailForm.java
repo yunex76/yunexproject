47c45146928f96a24d580249a176e209c6a4cb41
yunex76 <yunex76@bfb284a4-e940-11de-91d6-89caad0352f8>
2011-08-10 06:20:28 +0000

    1. 도서 (CW Android Programming tutorials) Tutorial 15 완료  - Feeding at Lunch  - RSS feed 리스트 표시

--- trunk/eclipse/workspaces_android/LunchList/src/com/yunex/DetailForm.java
+++ trunk/eclipse/workspaces_android/LunchList/src/com/yunex/DetailForm.java
@@ -1,13 +1,20 @@
 package com.yunex;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class DetailForm extends Activity {
 
@@ -15,6 +22,7 @@ public class DetailForm extends Activity {
 	EditText address = null;
 	EditText notes = null;
 	RadioGroup types = null;
+	EditText feed = null;
 	RestaurantHelper helper = null;
 	
 	String restaurantId = null;
@@ -30,6 +38,7 @@ public class DetailForm extends Activity {
         address = (EditText)findViewById(R.id.addr);
         types = (RadioGroup)findViewById(R.id.types);
         notes = (EditText)findViewById(R.id.notes);
+        feed = (EditText)findViewById(R.id.feed);
         
         Button save = (Button)findViewById(R.id.save);
 		save.setOnClickListener(onSave);
@@ -47,6 +56,7 @@ public class DetailForm extends Activity {
 		name.setText(helper.getName(c));
 		address.setText(helper.getAddress(c));
 		notes.setText(helper.getNotes(c));
+		feed.setText(helper.getFeed(c));
 		
 		if (helper.getType(c).equals("sit_down")) {
 			types.check(R.id.sit_down);
@@ -88,6 +98,36 @@ public class DetailForm extends Activity {
 		types.check(savedInstanceState.getInt("type"));
 	}
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.details_option, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.feed) {
+			if (isNetworkAvailable()) {
+				Intent i = new Intent(this, FeedActivity.class);
+				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
+				startActivity(i);
+			}
+			else {
+				Toast.makeText(this, "Sorry, the Internet is not available", Toast.LENGTH_LONG).show();
+			}
+			
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private boolean isNetworkAvailable() {
+		ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo info = cm.getActiveNetworkInfo();
+		return (info!=null);
+	}
+
 	private View.OnClickListener onSave = new OnClickListener() {
 		
 		@Override
@@ -108,12 +148,12 @@ public class DetailForm extends Activity {
 			
 			if (restaurantId == null) {
 				helper.insert(name.getText().toString(), address.getText().toString(),
-						type, notes.getText().toString());
+						type, notes.getText().toString(), feed.getText().toString());
 			}
 			else {
 				helper.update(restaurantId, name.getText().toString(),
 						address.getText().toString(),
-						type, notes.getText().toString());
+						type, notes.getText().toString(), feed.getText().toString());
 			}
 			
 			finish();
