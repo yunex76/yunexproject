db537c3c136331c8a432e581e6b4070c68030cc2
yunex76 <yunex76@bfb284a4-e940-11de-91d6-89caad0352f8>
2011-08-10 06:21:31 +0000

    Tutorial 15 release  - Feeding at Lunch  - RSS feed 리스트 표시

---
+++ tags/LunchList/0.0.15/src/com/yunex/EditPreferences.java
@@ -0,0 +1,13 @@
+package com.yunex;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class EditPreferences extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+}
