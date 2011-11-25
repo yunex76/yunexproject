6f1dd70b7ca03a12538af649f3afa6aa0904f2e0
yunex76 <yunex76@bfb284a4-e940-11de-91d6-89caad0352f8>
2011-11-25 09:42:04 +0000

    1. Junit / TDD / CI 오프라인 교육  - AuctionApp 소스 commit  - AuctionService 부분  - 265 페이지까지 소스

---
+++ trunk/eclipse/workspaces_junit-tdd-ci/AuctionApp/AuctionService/src/main/java/com/yunex/auction/service/dao/AuctionDao.java
@@ -0,0 +1,17 @@
+package com.yunex.auction.service.dao;
+
+import com.yunex.auction.service.obj.Auction;
+
+public class AuctionDao {
+
+	/**
+	 * 경매와 물품을 저장
+	 * @param sellingAuction
+	 * @return
+	 */
+	public long createAuctionAndItem(Auction sellingAuction) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
