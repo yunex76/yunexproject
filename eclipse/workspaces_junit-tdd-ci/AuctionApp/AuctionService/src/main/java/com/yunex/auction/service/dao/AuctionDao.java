7408aa8add80b86c03e65569f152ff672cec5317
yunex76 <yunex76@bfb284a4-e940-11de-91d6-89caad0352f8>
2011-11-29 00:19:31 +0000

    1. Junit / TDD / CI 오프라인 교육  - AuctionApp Project commit  - iBatis 추가  - 7일차 296 페이지까지 소스

--- eclipse/workspaces_junit-tdd-ci/AuctionApp/AuctionService/src/main/java/com/yunex/auction/service/dao/AuctionDao.java
+++ eclipse/workspaces_junit-tdd-ci/AuctionApp/AuctionService/src/main/java/com/yunex/auction/service/dao/AuctionDao.java
@@ -3,31 +3,19 @@ package com.yunex.auction.service.dao;
 import com.yunex.auction.service.obj.Auction;
 import com.yunex.auction.service.obj.AuctionStatus;
 
-public class AuctionDao {
+public interface AuctionDao {
 
 	/**
 	 * 경매와 물품을 저장
 	 * @param sellingAuction
 	 * @return
 	 */
-	public long createAuctionAndItem(Auction sellingAuction) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	public long createAuctionAndItem(Auction sellingAuction);
 
-	public Auction findAuctionById(long l) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Auction findAuctionById(long l);
 
-	public void updateAuction(Auction auction) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void updateAuction(Auction auction);
 
-	public AuctionStatus findAuctionStatusById(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public AuctionStatus findAuctionStatusById(long id);
 
 }
