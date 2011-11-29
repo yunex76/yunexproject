9fda3e3ba301a28feadebe2367a644144e9539fb
yunex76 <yunex76@bfb284a4-e940-11de-91d6-89caad0352f8>
2011-11-29 08:24:34 +0000

    스프링3 레시피 책 751 ~ 757페이지까지 commit  - JPA 적용

---
+++ trunk/eclipse/workspaces_spring3/SpringTest/src/main/java/com/yunex/springrecipes/course/JpaCourseDao.java
@@ -0,0 +1,80 @@
+package com.yunex.springrecipes.course;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class JpaCourseDao implements CourseDao {
+
+	private EntityManagerFactory entityManagerFactory;
+	
+	public JpaCourseDao() {
+		entityManagerFactory = Persistence.createEntityManagerFactory("course");
+	}
+	
+	@Override
+	public void store(Course course) {
+		EntityManager manager = entityManagerFactory.createEntityManager();
+		EntityTransaction tx = manager.getTransaction();
+		
+		try {
+			tx.begin();
+			manager.merge(course);
+			tx.commit();
+		}
+		catch (RuntimeException e) {
+			tx.rollback();
+			throw e;
+		}
+		finally {
+			manager.close();
+		}
+	}
+
+	@Override
+	public void delete(Long courseId) {
+		EntityManager manager = entityManagerFactory.createEntityManager();
+		EntityTransaction tx = manager.getTransaction();
+		
+		try {
+			tx.begin();
+			Course course = manager.find(Course.class, courseId);
+			tx.commit();
+		}
+		catch (RuntimeException e) {
+			tx.rollback();
+			throw e;
+		}
+		finally {
+			manager.close();
+		}
+	}
+
+	@Override
+	public Course findById(Long courseId) {
+		EntityManager manager = entityManagerFactory.createEntityManager();
+		try {
+			return manager.find(Course.class, courseId);
+		}
+		finally {
+			manager.close();
+		}
+	}
+
+	@Override
+	public List<Course> findAll() {
+		EntityManager manager = entityManagerFactory.createEntityManager();
+		try {
+			Query query = manager.createQuery("select course from Course course");
+			return query.getResultList();
+		}
+		finally {
+			manager.close();
+		}
+	}
+
+}
