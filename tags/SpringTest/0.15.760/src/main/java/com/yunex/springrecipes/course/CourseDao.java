a1a5182c7340e32e5fb05959aaddfff6a4690b34
yunex76 <yunex76@bfb284a4-e940-11de-91d6-89caad0352f8>
2011-11-30 05:17:54 +0000

    스프링3 레시피 책 758 ~ 760페이지까지 tag  - 스프링에서 하이버네이트 세션팩터리 설정하기 일부 부분

---
+++ tags/SpringTest/0.15.760/src/main/java/com/yunex/springrecipes/course/CourseDao.java
@@ -0,0 +1,11 @@
+package com.yunex.springrecipes.course;
+
+import java.util.List;
+
+public interface CourseDao {
+
+	public void store(Course course);
+	public void delete(Long courseId);
+	public Course findById(Long courseId);
+	public List<Course> findAll();
+}
