03bf4e801a8aeff46400b5af7c24cc6a411ffd9d
yunex76 <yunex76@bfb284a4-e940-11de-91d6-89caad0352f8>
2011-12-09 06:23:53 +0000

    KimisTest Project commit  - 전세자금대출이율 관련 테스트 추가    - 하이버네이트를 이용한 DB 등록 (MS-SQL)    - 등록/수정    - Unique Key 조회 기능

--- trunk/eclipse/workspaces_junit-tdd-ci/KimisTest/src/main/java/com/kyoborealco/hrm/entity/LoanHouseDepositRate.java
+++ trunk/eclipse/workspaces_junit-tdd-ci/KimisTest/src/main/java/com/kyoborealco/hrm/entity/LoanHouseDepositRate.java
@@ -2,56 +2,87 @@ package com.kyoborealco.hrm.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
 /**
  * 전세자금대출이율 entity
  * @author 남윤혁
  * @since 2011-12-01
  */
+@NamedQueries({
+
+	// Unique Key를 이용해서 조회
+	@NamedQuery(
+		name = "findByUK",
+		query = "from LoanHouseDepositRate where startUseDate = :startUseDate and endUseDate = :endUseDate "
+	),
+})
+
+@Entity
+@Table(name = "TH_LHDRXM")
 public class LoanHouseDepositRate {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "LHDR_NUMB", columnDefinition="int")
 	private long id;
 	
 	/**
 	 * 사용시작일자
 	 * ex) 2011년12월01일 => 20111201
 	 */
+	@Column(name = "SUSE_DATE", length = 8, nullable = false, columnDefinition = "char(8)")
 	private String startUseDate;
 	
 	/**
 	 * 사용종료일자
 	 * ex) 2011년12월31일 => 20111231
 	 */
+	@Column(name = "EUSE_DATE", length = 8, nullable = false, columnDefinition = "char(8)")
 	private String endUseDate;
 	
 	/**
 	 * 회사설정이율
 	 */
+	@Column(name = "LHDE_RATE", nullable = false, columnDefinition="numeric(5,3)")
 	private double companyRate;
 
 	/**
 	 * 국세청이율
 	 * irs -> Internal Revenue Service
 	 */
+	@Column(name = "TAXA_RATE", nullable = false, columnDefinition="numeric(5,3)")
 	private double irsRate;
 	
 	/**
 	 * 데이터생성 사용자ID
 	 */
+	@Column(name = "REEM_CODE", length = 20)
 	private String createUserId;
 	
 	/**
 	 * 데이터생성 시간
 	 */
+	@Column(name = "REGI_DATE")
 	private Date createDateTime;
 	
 	/**
 	 * 데이터 최종수정 사용자ID
 	 */
+	@Column(name = "UPEM_CODE", length = 20)
 	private String updateUserId;
 	
 	/**
 	 * 데이터 최종수정 시간
 	 */
+	@Column(name = "UPDA_DATE")
 	private Date updateDateTime;
 
 	public LoanHouseDepositRate() {
