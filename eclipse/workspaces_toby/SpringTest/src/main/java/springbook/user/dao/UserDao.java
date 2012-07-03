9f1b13d300f541692db233cca7f3a97e86c1af54
yunex76 <yunex76@bfb284a4-e940-11de-91d6-89caad0352f8>
2012-07-03 09:59:30 +0000

    1. UserDao의 Connection 객체를 별도 함수로 분리 (page 65)

--- eclipse/workspaces_toby/SpringTest/src/main/java/springbook/user/dao/UserDao.java
+++ eclipse/workspaces_toby/SpringTest/src/main/java/springbook/user/dao/UserDao.java
@@ -11,8 +11,7 @@ import springbook.user.domain.User;
 public class UserDao {
 
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Class.forName("org.hsqldb.jdbcDriver");
-		Connection c = DriverManager.getConnection("jdbc:hsqldb:D:\\MyStudy\\Spring3_Toby\\DB", "sa", "");
+		Connection c = getConnection();
 
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?, ?, ?)");
 		ps.setString(1, user.getId());
@@ -26,8 +25,7 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Class.forName("org.hsqldb.jdbcDriver");
-		Connection c = DriverManager.getConnection("jdbc:hsqldb:D:\\MyStudy\\Spring3_Toby\\DB", "sa", "");
+		Connection c = getConnection();
 
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
@@ -44,5 +42,12 @@ public class UserDao {
 		c.close();
 		
 		return user;
+	}
+
+	private Connection getConnection() throws ClassNotFoundException,
+			SQLException {
+		Class.forName("org.hsqldb.jdbcDriver");
+		Connection c = DriverManager.getConnection("jdbc:hsqldb:D:\\MyStudy\\Spring3_Toby\\DB", "sa", "");
+		return c;
 	}
 }
